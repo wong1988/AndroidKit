cd0119698f0bd1f418d9ddc966e5672ff2c5b921
wong1988 <94215397+wong1988@users.noreply.github.com>
2021-12-16 22:02:46 +0800

    Create PolyphonicDict.java

---
+++ AndroidKit/src/main/java/io/github/wong1988/kit/lexicons/PolyphonicDict.java
@@ -0,0 +1,31 @@
+package io.github.wong1988.kit.lexicons;
+
+import android.content.Context;
+
+public class PolyphonicDict extends AndroidAssetDict {
+
+    static volatile PolyphonicDict singleton = null;
+
+    public PolyphonicDict(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected String assetFileName() {
+        return "polyphonic.txt";
+    }
+
+    public static PolyphonicDict getInstance(Context context) {
+        if (context == null) {
+            throw new IllegalArgumentException("context == null");
+        }
+        if (singleton == null) {
+            synchronized (PolyphonicDict.class) {
+                if (singleton == null) {
+                    singleton = new PolyphonicDict(context);
+                }
+            }
+        }
+        return singleton;
+    }
+}
