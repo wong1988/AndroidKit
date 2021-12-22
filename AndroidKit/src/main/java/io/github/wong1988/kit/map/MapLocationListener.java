7fe3e06b8418fcc91a36b17757cfde86e5e2d3c2
wong1988 <94215397+wong1988@users.noreply.github.com>
2021-12-23 06:34:37 +0800

    Update MapLocationListener.java

--- AndroidKit/src/main/java/io/github/wong1988/kit/map/MapLocationListener.java
+++ AndroidKit/src/main/java/io/github/wong1988/kit/map/MapLocationListener.java
@@ -5,5 +5,5 @@ import io.github.wong1988.kit.entity.MapLocation;
 public interface MapLocationListener {
 
     // mapLocation == null error
-    void location(MapLocation mapLocation, String error);
+    void onLocationChanged(MapLocation mapLocation, String error);
 }
