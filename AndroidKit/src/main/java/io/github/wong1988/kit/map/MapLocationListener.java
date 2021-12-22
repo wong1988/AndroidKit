8e27618ffff93bf7ddbd3feaab89f714b688a6a1
wong1988 <94215397+wong1988@users.noreply.github.com>
2021-12-23 07:03:08 +0800

    Update MapLocationListener.java

--- AndroidKit/src/main/java/io/github/wong1988/kit/map/MapLocationListener.java
+++ AndroidKit/src/main/java/io/github/wong1988/kit/map/MapLocationListener.java
@@ -4,6 +4,9 @@ import io.github.wong1988.kit.entity.MapLocation;
 
 public interface MapLocationListener {
 
-    // mapLocation == null error
-    void onLocationChanged(MapLocation mapLocation, String error);
+    // errorCode == -1 success
+    // errorCode == 0 PASSIVE禁用
+    // errorCode == 1 GPS禁用
+    // errorCode == 2 NETWORK禁用
+    void onLocationChanged(MapLocation mapLocation, int errorCode);
 }
