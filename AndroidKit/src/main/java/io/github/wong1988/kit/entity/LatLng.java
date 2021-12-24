a9ebc6f06ffe7abd43406fcadd144c6b8559234d
wong1988 <94215397+wong1988@users.noreply.github.com>
2021-12-24 20:28:51 +0800

    Create LatLng.java

---
+++ AndroidKit/src/main/java/io/github/wong1988/kit/entity/LatLng.java
@@ -0,0 +1,31 @@
+package io.github.wong1988.kit.entity;
+
+/**
+ * 坐标
+ */
+public class LatLng {
+
+    public final double latitude;
+    public final double longitude;
+
+    public LatLng(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "LatLng{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
