5c86cc0fac4e95cf5f7af3466280abb4280514d1
wong1988 <94215397+wong1988@users.noreply.github.com>
2021-12-24 20:23:44 +0800

    Create CoordinateSystem.java

---
+++ AndroidKit/src/main/java/io/github/wong1988/kit/type/CoordinateSystem.java
@@ -0,0 +1,24 @@
+package io.github.wong1988.kit.type;
+
+public enum CoordinateSystem {
+
+    WGS84("地球坐标", "国际地图提供商，谷歌国际地图"),
+    GCJ02("火星坐标，国测局坐标", "高德地图，谷歌地图，腾讯地图，阿里云地图"),
+    BD09("百度坐标", "百度地图");
+
+    private String alias;
+    private String map;
+
+    CoordinateSystem(String alias, String map) {
+        this.alias = alias;
+        this.map = map;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public String getMap() {
+        return map;
+    }
+}
