b8a1da32266d37a0b7067c528f4ffa8146bd8ce8
wong1988 <wong1988@petalmail.com>
2022-01-18 11:55:12 +0800

    Update UiUtils.java

--- AndroidKit/src/main/java/io/github/wong1988/kit/utils/UiUtils.java
+++ AndroidKit/src/main/java/io/github/wong1988/kit/utils/UiUtils.java
@@ -1,5 +1,6 @@
 package io.github.wong1988.kit.utils;
 
+import android.content.Context;
 import android.util.TypedValue;
 
 import io.github.wong1988.kit.AndroidKit;
@@ -8,9 +9,17 @@ public class UiUtils {
 
     /**
      * dp转像素
+     * 注意：自定义View如果想实时预览请调用2参的方法
      */
     public static float dip2px(float dp) {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, AndroidKit.getInstance().getAppContext().getResources().getDisplayMetrics());
+    }
+
+    /**
+     * dp转像素
+     */
+    public static float dip2px(Context context, float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
     }
 
 }
