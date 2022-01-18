758f350e4920a0a991bd0d46306af3e4f632087e
wong1988 <wong1988@petalmail.com>
2022-01-18 10:49:25 +0800

    Update CoordinateSystem.java

--- AndroidKit/src/main/java/io/github/wong1988/kit/type/CoordinateSystem.java
+++ AndroidKit/src/main/java/io/github/wong1988/kit/type/CoordinateSystem.java
@@ -9,19 +9,6 @@ public enum CoordinateSystem {
     GCJ02("火星坐标，国测局坐标", "高德地图，谷歌地图，腾讯地图，阿里云地图"),
     BD09("百度坐标", "百度地图");
 
-    private String alias;
-    private String map;
-
     CoordinateSystem(String alias, String map) {
-        this.alias = alias;
-        this.map = map;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public String getMap() {
-        return map;
     }
 }
