ebea737306eabf724eb195cd4b0cba834c9139fe
wong1988 <wong1988@petalmail.com>
2022-12-01 17:07:06 +0800

    1.1.0.2

--- app/src/main/java/io/github/kit/example/activity/EditTextActivity.java
+++ app/src/main/java/io/github/kit/example/activity/EditTextActivity.java
@@ -1,10 +1,14 @@
 package io.github.kit.example.activity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import io.github.kit.example.R;
+import io.github.wong1988.kit.filter.CashierListener;
+import io.github.wong1988.kit.utils.EditTextUtils;
 
 public class EditTextActivity extends AppCompatActivity {
 
@@ -12,5 +16,23 @@ public class EditTextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
+
+        EditText cash = findViewById(R.id.cash);
+        EditText chinese = findViewById(R.id.chinese);
+        TextView noticeCash = findViewById(R.id.noticeCash);
+
+        EditTextUtils.setCashFilter(cash, "111", new CashierListener() {
+            @Override
+            public void overMax(String max, String fillZeroMaxValue) {
+                noticeCash.setText("超出最大值：" + fillZeroMaxValue);
+            }
+
+            @Override
+            public void correct(String value, String fillZeroValue) {
+                noticeCash.setText(fillZeroValue);
+            }
+        });
+
+        EditTextUtils.setChineseFilter(chinese);
     }
 }
