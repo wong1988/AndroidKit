33296bfa48822420f0d10b24439e7ca21d7f0868
wong1988 <wong1988@petalmail.com>
2023-02-15 14:49:54 +0800

    Update WifiApAddress.java

--- AndroidKit/src/main/java/io/github/wong1988/kit/task/WifiApAddress.java
+++ AndroidKit/src/main/java/io/github/wong1988/kit/task/WifiApAddress.java
@@ -5,7 +5,7 @@ public enum WifiApAddress {
     NORMAL("大部分手机可用，高版本荣耀、鸿蒙会跳转到《更多共享设置》", "com.android.settings.Settings$TetherSettingsActivity"),
     SPECIAL("高版本荣耀、鸿蒙使用《移动网络设置》进行热点设置", "com.android.settings.Settings$WirelessSettingsActivity");
 
-    private String address;
+    private final String address;
 
     WifiApAddress(String describe, String address) {
         this.address = address;
